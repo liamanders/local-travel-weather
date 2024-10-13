60de5676da89eb7a1e7cc8f0011b514160aac7a3
Gloria Morales <gloria.morales@happysocks.com>
2024-10-13 22:39:06 +0200

    Add Express package

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+const corsOptions = {
+  origin: ["http://localhost:5173"], //where the app is running
+};
+
+app.use(cors(corsOptions));
+
+app.get("/api", (req, res) => {
+  res.json({}); //the response goes here
+});
+
+app.listen(8080, () => {
+  console.log("Server running on port 8080");
+});
