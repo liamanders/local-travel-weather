d9b59a13d921b9be84fbf0c77df3a84601175377
Gloria Morales <gloria.morales@happysocks.com>
2024-10-16 21:19:38 +0200

    Add API endpoint for retrieving train departures

--- server/server.js
+++ server/server.js
@@ -1,16 +1,15 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
+const departures = require("./departures");
+
+const app = express();
 
 const corsOptions = {
   origin: ["http://localhost:5173"], //where the app is running
 };
-
 app.use(cors(corsOptions));
 
-app.get("/api", (req, res) => {
-  res.json({}); //the response goes here
-});
+app.use("/api/departures", departures);
 
 app.listen(8080, () => {
   console.log("Server running on port 8080");
