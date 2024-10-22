ce1fc05ee76370d1d8c3fe651d56c520ad6a3bd0
Liam Andersson <contactliamandersson@gmail.com>
2024-10-22 18:25:02 +0200

    Geocode component

--- server/server.js
+++ server/server.js
@@ -1,21 +1,32 @@
 const express = require("express");
 const cors = require("cors");
 const departures = require("./departures");
+const geoCode = require("./geoLocationApi");
 
 require("dotenv").config();
 
 const app = express();
 
+const port = 8080;
+
 const corsOptions = {
   origin: ["http://localhost:5173"], //where the app is running
 };
 
-app.use(cors(corsOptions));
+app.use(express.json(), cors(corsOptions));
+
+app.get("/geocode", async (req, res) => {
+  try {
+    const address = req.body.address;
+    const coordinates = await geoCode(address);
+    res.json(coordinates);
+  } catch (error) {
+    res.status(500).json({ error: "Geocoding failed" });
+  }
+});
 
 app.use("/api/departures", departures);
 
-// When someone visits URL: http://localhost:8080/api/departures, the server will respond
-// run server: npm run server
-app.listen(8080, () => {
-  console.log("Server running on port 8080");
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
