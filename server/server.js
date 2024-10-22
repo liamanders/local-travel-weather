658cfe61ef81d763d4d6cf3269ff9c3d58e63e33
Gloria Morales <gmoraleslondono@gmail.com>
2024-10-22 20:08:56 +0200

    Fetch departures data (#23)
    * Add API endpoint for retrieving near station departures

--- server/server.js
+++ server/server.js
@@ -5,17 +5,21 @@ require("dotenv").config();
 
 const app = express();
 const cors = require("cors");
+const departures = require("./departures");
+
+require("dotenv").config();
+
+const app = express();
 
 const corsOptions = {
   origin: ["http://localhost:5173"], //where the app is running
 };
-
 app.use(cors(corsOptions));
 
-app.get("/api", (req, res) => {
-  res.json({}); //the response goes here
-});
+app.use("/api/departures", departures);
 
+// When someone visits URL: http://localhost:8080/api/departures, the server will respond
+// run server: npm run server
 app.listen(8080, () => {
   console.log("Server running on port 8080");
 });
