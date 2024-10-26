eecbd97efa2bc305c00a49f2a92f83e92fd07644
Liam Andersson <contactliamandersson@gmail.com>
2024-10-26 08:47:59 +0200

    Fixed formstyle and added loading status and logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import { useState } from 'react';
 import './App.css'
+import logo from './assets/logo.png'
 
 function App() {
 
-  const [address, setAddress] = useState('Sundsgårdens Folkhögskola');
+  const [address, setAddress] = useState('');
   const [coordinates, setCoordinates] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   
   
-
-
     const handleGeocode  = async (e: React.FormEvent) => {
       e.preventDefault();
+      setLoading(true);
+      setTimeout(async () => {
       try {
         const response = await fetch('http://localhost:8080/geocode', {
           method: 'POST',
@@ -25,8 +27,11 @@ function App() {
         setCoordinates(data);
       } catch (error) {
         console.error('Error fetching coordinates:', error);
+      } finally {
+        setLoading(false);
       }
-    };
+    }, 1000);
+    }; 
 
   
 
@@ -34,15 +39,16 @@ function App() {
       <>
         <header>
           <div className='logo'>
-
+            <img className= 'logoImg' src= {logo} alt="Logo" />
           </div>
           <div className='title'>
-            <h1>Local, Travel and Weather</h1>
+            <h1><span className='local'>Local, </span><span className='travel'>Travel and Weather</span></h1>
           </div>
           <div className='searchBar'>
             <form className="search-form" onSubmit={handleGeocode}>
               <input type="text" value={address} onChange={(e) => setAddress(e.target.value)} placeholder="Enter Your Address" required/>
-              <button type='submit'>SEARCH &nbsp; <i className="fa fa-search"></i></button>
+              <button type='submit'
+              disabled={loading}>{loading ? 'Loading ...' : 'SEARCH'} <i className="fa fa-search"></i></button>
               <h3>{coordinates && (
               <p>Latitude: {coordinates.latitude}, Longitude: {coordinates.longitude}</p>
                 )}</h3>
