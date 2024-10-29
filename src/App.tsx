612715295fee13a8939ede220427181ec66f2b8f
Liam Andersson <contactliamandersson@gmail.com>
2024-10-29 19:32:55 +0100

    Solved conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,44 @@
-
 import { useState } from 'react';
+import './App.css'
+import logo from './assets/logo.png'
 
-import "./App.css";
-import { Departures } from "./Departures";
 import GeoCoding from './GeoCoding'
-
+import { Departures } from './Departures';
 
 function App() {
 
 
-
   
 
-
   return (
-    <>
-      <header>
-        <div className="logo"></div>
+      <>
+        <header>
+          <div className='header'>
+        <div className="logo">
+        <a href="/"><img className= 'logoImg' src= {logo} alt="Logo" /></a>
+        </div>
         <div className="title">
-          <h1>Local, Travel and Weather</h1>
+        <h1><span className='local'>Local, </span><span className='travel'>Travel and Weather</span></h1>
         </div>
-        <GeoCoding />
-      </header>
-      <main>
-        <div className="divider"></div>
-        <div className="genContainer">
-          <div className="transportCont">
-            <Departures />
-          </div>
-          <div className="localCont">
-            <h2>LOCAL WEATHER</h2>
-          </div>
-          <div className="trafficCont">
-            <h2>TRAFFIC UPDATES</h2>
-          </div>
         </div>
-      </main>
-    </>
-  );
-
+          <GeoCoding />
+        </header>
+        <main>
+          <div className='divider'></div>
+          <div className='genContainer'>
+            <div className='transportCont'>
+              <Departures />
+            </div>
+            <div className='localCont'>
+              <h2>LOCAL WEATHER</h2>
+            </div>
+            <div className='trafficCont'>
+              <h2>TRAFFIC UPDATES</h2>
+            </div>
+          </div>
+        </main>
+      </>
+  )
 }
 
-export default App;
+export default App
