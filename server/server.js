0404f3d90eb8f6002ddbf91614a8b77cc7483803
Gloria Morales <gmoraleslondono@gmail.com>
2024-10-29 18:38:42 +0100

    Departures UI (#26)
    Departures UI

--- server/server.js
+++ server/server.js
@@ -1,23 +1,19 @@
 const express = require("express");
-
-const app = express();
 const cors = require("cors");
-
 const departures = require("./departures");
-
-const geoCode = require('./geoLocationApi');
 
 require("dotenv").config();
 
-const port = 8080;
+const app = express();
 
+const geoCode = require('./geoLocationApi');
+
+const port = 8080;
 
 
 const corsOptions = {
 origin: ["http://localhost:5173"], //where the app is running
 };
-
-require("dotenv").config();
 
 app.use(express.json(), cors(corsOptions));
 
